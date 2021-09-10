04fe06344feb91c2dff8db757ab313f66b0034c2
Brandon Dyer <brandondyer64@gmail.com>
2021-09-10 15:49:17 -0500

    #3 | Replies (#8)
    * Better replies
    
    * Renamed comments to replies

--- hooks/lotide.ts
+++ hooks/lotide.ts
@@ -1,53 +1,90 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 export interface Post {
-    author: Author;
-    community: Community;
-    content_html: string;
-    content_text?: string;
-    created: string;
-    href: string;
-    id: number;
-    replies_count_total: number;
-    score: number;
-    sticky: boolean;
-    title: string;
+  author: Author;
+  community: Community;
+  content_html: string;
+  content_text?: string;
+  created: string;
+  href: string;
+  id: number;
+  replies_count_total: number;
+  score: number;
+  sticky: boolean;
+  title: string;
 }
 
 export interface Author {
-    host: string;
-    id: number;
-    local: boolean;
-    remote_url?: string;
-    username: string
+  host: string;
+  id: number;
+  local: boolean;
+  remote_url?: string;
+  username: string;
+  avatar?: {
+    url: string;
+  };
 }
 
 export interface Community {
-    host: string;
-    id: number;
-    local: boolean;
-    name: string;
-    remote_url?: string;
+  host: string;
+  id: number;
+  local: boolean;
+  name: string;
+  remote_url?: string;
+}
+
+export interface Replies {
+  items: Reply[];
+  next_page: string;
+}
+
+export interface Reply {
+  id: number;
+  content_text: string;
+  content_html: string;
+  attachments: [
+    {
+      url: string;
+    }
+  ];
+  author: Author;
+  created: string;
+  deleted: boolean;
+  local: boolean;
+  replies: Replies;
+  your_vote: {};
+  score: number;
 }
 
 export function usePosts(refreshCount: number): Post[] {
-    const [posts, setPosts] = useState([] as any[]);
-    useEffect(() => {
-        console.log("fetching");
-        fetch(
-            "https://hoot.goldandblack.xyz/api/unstable/posts",
-            {
-                method: 'GET',
-                headers: {
-                    'Authorization': 'Bearer '
-                }
-            }
-        )
-            .then(data => data.json())
-            .then(data => {
-                // console.log(data);
-                setPosts(data);
-            });
-    }, [refreshCount]);
-    return posts;
+  const [posts, setPosts] = useState([] as any[]);
+  useEffect(() => {
+    fetch("https://hoot.goldandblack.xyz/api/unstable/posts", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer ",
+      },
+    })
+      .then((data) => data.json())
+      .then((data) => {
+        // console.log(data);
+        setPosts(data);
+      });
+  }, [refreshCount]);
+  return posts;
+}
+
+export function useReplies(postId: number): Replies {
+  const [replies, setReplies] = useState({
+    items: [] as Reply[],
+  } as Replies);
+  useEffect(() => {
+    fetch(`https://hoot.goldandblack.xyz/api/unstable/posts/${postId}/replies`)
+      .then((data) => data.json())
+      .then((data) => {
+        console.log(JSON.stringify(data, null, 2));
+        setReplies(data);
+      });
+  }, []);
+  return replies;
 }
