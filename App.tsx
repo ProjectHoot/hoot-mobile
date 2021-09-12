35935b5af30d79e51cb28fe53f5176f338a0a793
Brandon Dyer <brandondyer64@gmail.com>
2021-09-12 14:49:02 -0500

    Better login context

--- App.tsx
+++ App.tsx
@@ -5,18 +5,18 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
-import LoginContext, { defaultStore } from "./store/LoginContext";
+import LoginContext, { defaultStore, Login } from "./store/LoginContext";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
-  const [login, setLogin] = useState(defaultStore);
+  const [login, setLogin] = useState<Login | undefined>(defaultStore);
 
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
-      <LoginContext.Provider value={{ login, setLogin }}>
+      <LoginContext.Provider value={{ login, setLogin: (login: Login) => setLogin(login) }}>
         <SafeAreaProvider>
           <Navigation colorScheme={colorScheme} />
           <StatusBar />
--- store/LoginContext.ts
+++ store/LoginContext.ts
@@ -12,15 +12,7 @@ export interface User {
   username: string;
 }
 
-export const defaultStore: Login | undefined = {
-  token: "5beac9ac-5c0c-447d-98f9-fb3b6066f076",
-  user: {
-    has_unread_notifications: false,
-    id: 1176,
-    is_site_admin: false,
-    username: "bd64",
-  },
-};
+export const defaultStore = undefined;
 
 export default React.createContext({
   login: defaultStore as Login | undefined,
