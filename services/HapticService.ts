4cc0554906c5c1963e6bf6979a0bf142c2522063
Brandon Dyer <brandondyer64@gmail.com>
2021-09-18 14:50:31 -0500

    #139 | Fixed mobile haptics error (#142)

--- services/HapticService.ts
+++ services/HapticService.ts
@@ -1,9 +1,9 @@
 import { Platform } from "react-native";
 import * as Haptics from "expo-haptics";
 
-export import ImpactFeedbackStyle = Haptics.ImpactFeedbackStyle;
+export { ImpactFeedbackStyle } from "expo-haptics";
 
-export async function impactAsync(style: ImpactFeedbackStyle) {
+export async function impactAsync(style: Haptics.ImpactFeedbackStyle) {
   if (Platform.OS === "web") return;
   await Haptics.impactAsync(style);
 }
