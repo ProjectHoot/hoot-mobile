0842e3ac1611f749369bd9b932d38dc6afb2d95e
Brandon Dyer <brandondyer64@gmail.com>
2021-09-17 20:36:30 -0500

    #63 | Nested replies (#124)

---
+++ components/RepliesDisplay.tsx
@@ -0,0 +1,225 @@
+import React, { useContext, useState } from "react";
+import { ColorValue, Pressable } from "react-native";
+import Icon from "@expo/vector-icons/Ionicons";
+import { View, Text } from "./Themed";
+import useTheme from "../hooks/useTheme";
+import * as Haptics from "expo-haptics";
+import ElapsedTime from "./ElapsedTime";
+import HTMLView from "react-native-htmlview";
+import * as LotideService from "../services/LotideService";
+import LotideContext from "../store/LotideContext";
+
+export interface RepliesDisplayProps {
+  replies: Paged<Reply>;
+  navigation: any;
+  layer?: number;
+  postId?: PostId;
+  replyId?: ReplyId;
+}
+
+export default function RepliesDisplay({
+  replies,
+  navigation,
+  layer = 0,
+  postId,
+  replyId,
+}: RepliesDisplayProps) {
+  const [nextPageData, setNextPageData] = useState<Paged<Reply>>();
+  const theme = useTheme();
+  const ctx = useContext(LotideContext).ctx;
+  const layerColors = [
+    theme.text,
+    theme.red,
+    theme.orange,
+    theme.yellow,
+    theme.green,
+    theme.teal,
+    theme.blue,
+    theme.indigo,
+    theme.purple,
+  ];
+
+  return (
+    <View>
+      {replies.items.map(reply => (
+        <ReplyDisplay
+          reply={reply}
+          layer={layer}
+          key={reply.id}
+          navigation={navigation}
+          layerColors={layerColors}
+          postId={postId}
+        />
+      ))}
+      {replies.next_page !== null &&
+        nextPageData === undefined &&
+        (postId || replyId) && (
+          <Pressable
+            hitSlop={5}
+            onPress={() => {
+              console.log(replies.next_page);
+              if (replyId) {
+                LotideService.getReplyReplies(
+                  ctx,
+                  replyId,
+                  replies.next_page,
+                ).then(setNextPageData);
+              } else if (postId) {
+                LotideService.getPostReplies(
+                  ctx,
+                  postId,
+                  replies.next_page,
+                ).then(setNextPageData);
+              }
+            }}
+          >
+            <Text
+              style={{ color: theme.tint, paddingTop: 5, paddingBottom: 10 }}
+            >
+              More replies <Icon name="chevron-down-outline" />
+            </Text>
+          </Pressable>
+        )}
+      {nextPageData !== undefined && (
+        <RepliesDisplay
+          replies={nextPageData}
+          navigation={navigation}
+          layer={layer}
+          postId={postId}
+          replyId={replyId}
+        />
+      )}
+      {replies.next_page === null && layer === 0 && (
+        <Text style={{ margin: 17, color: theme.secondaryText }}>
+          {replies.items.length > 0 ? "No more replies" : "No replies yet"}
+        </Text>
+      )}
+    </View>
+  );
+}
+
+function ReplyDisplay({
+  reply,
+  layer = 0,
+  navigation,
+  layerColors,
+  postId,
+}: {
+  reply: Reply;
+  layer: number;
+  navigation: any;
+  layerColors: ColorValue[];
+  postId?: PostId;
+}) {
+  const [nextPageData, setNextPageData] = useState<Paged<Reply>>();
+  const [showChildren, setShowChildren] = React.useState(true);
+  const theme = useTheme();
+  const ctx = useContext(LotideContext).ctx;
+
+  return (
+    <View style={{ paddingLeft: 0 }}>
+      <View
+        style={{
+          paddingVertical: 8,
+          borderTopWidth: 0.5,
+          borderTopColor: theme.secondaryBackground,
+        }}
+      >
+        <Pressable
+          onPress={() => setShowChildren(s => !s)}
+          onLongPress={() => {
+            Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
+            navigation.navigate("Reply", {
+              id: reply.id,
+              title: reply.author.username,
+              html: reply.content_html,
+              type: "reply",
+            });
+          }}
+        >
+          <View
+            style={{
+              borderLeftWidth: 2,
+              borderColor: layerColors[layer % layerColors.length],
+              paddingLeft: 15,
+              paddingVertical: 3,
+            }}
+          >
+            <Text
+              style={{
+                color: theme.text,
+                fontSize: 16,
+                marginBottom: 5,
+                fontWeight: "500",
+              }}
+            >
+              {reply.author.username}
+              {"  "}
+              <Text style={{ color: theme.text, fontSize: 14 }}>
+                <Icon name="heart-outline" size={14} color={theme.text} light />{" "}
+                {reply.score}
+                {"   "}
+                <ElapsedTime time={reply.created} />
+              </Text>
+            </Text>
+            {showChildren && !!reply.content_html && (
+              <HTMLView
+                value={reply.content_html
+                  .replace(/<hr>/g, "")
+                  .replace(/\n/g, "")}
+                stylesheet={{
+                  p: { color: theme.text, fontSize: 16, fontWeight: "300" },
+                  h1: {
+                    color: theme.text,
+                    fontSize: 24,
+                    fontWeight: "300",
+                  },
+                }}
+              />
+            )}
+          </View>
+        </Pressable>
+      </View>
+      {reply.replies !== null
+        ? reply.replies.items.length > 0 &&
+          showChildren && (
+            <View style={{ paddingLeft: 15 }}>
+              <RepliesDisplay
+                replies={reply.replies}
+                layer={layer + 1}
+                navigation={navigation}
+                postId={postId}
+                replyId={reply.id}
+              />
+            </View>
+          )
+        : nextPageData === undefined && (
+            <Pressable
+              hitSlop={5}
+              onPress={() => {
+                LotideService.getReplyReplies(ctx, reply.id).then(
+                  setNextPageData,
+                );
+              }}
+            >
+              <View style={{ paddingHorizontal: 15, paddingBottom: 10 }}>
+                <Text style={{ color: theme.tint }}>
+                  More replies <Icon name="chevron-forward-outline" />
+                </Text>
+              </View>
+            </Pressable>
+          )}
+      {nextPageData !== undefined && (
+        <View style={{ paddingLeft: 15 }}>
+          <RepliesDisplay
+            replies={nextPageData}
+            navigation={navigation}
+            layer={layer + 1}
+            postId={postId}
+            replyId={reply.id}
+          />
+        </View>
+      )}
+    </View>
+  );
+}
--- screens/ModalScreen.tsx
+++ screens/ModalScreen.tsx
@@ -1,5 +1,5 @@
 import Icon from "@expo/vector-icons/Ionicons";
-import React, { useContext, useEffect, useMemo, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import {
   StyleSheet,
   StatusBar,
@@ -7,15 +7,14 @@ import {
   Pressable,
   Share,
 } from "react-native";
-import HTMLView from "react-native-htmlview";
 import * as Haptics from "expo-haptics";
-import ElapsedTime from "../components/ElapsedTime";
 import PostDisplay from "../components/PostDisplay";
-import { Text, View } from "../components/Themed";
+import { View } from "../components/Themed";
 import { useReplies } from "../hooks/lotide";
 import useTheme from "../hooks/useTheme";
 import { RootStackScreenProps } from "../types";
 import LotideContext from "../store/LotideContext";
+import RepliesDisplay from "../components/RepliesDisplay";
 
 export default function ModalScreen({
   navigation,
@@ -77,14 +76,12 @@ export default function ModalScreen({
             <Icon name="share-outline" size={25} color={theme.text} />
           </Pressable>
         </View>
-        <RepliesDisplay replies={replies} navigation={navigation} />
-      </View>
-      <View>
-        <Text style={{ textAlign: "center" }}>
-          {"\n"}
-          {replies.items.length === 0 ? "No replies yet" : "No more replies"}
-          {"\n\n\n\n"}
-        </Text>
+        <RepliesDisplay
+          replies={replies}
+          navigation={navigation}
+          postId={post.id}
+        />
+        <View style={{ height: 300 }} />
       </View>
     </ScrollView>
   );
@@ -139,128 +136,3 @@ const styles = StyleSheet.create({
     padding: 10,
   },
 });
-
-function RepliesDisplay({
-  replies,
-  layer = 0,
-  navigation,
-}: {
-  replies: Paged<Reply>;
-  layer?: number;
-  navigation: any;
-}) {
-  return (
-    <View>
-      {replies.items.map(reply => (
-        <ReplyDisplay
-          reply={reply}
-          layer={layer}
-          key={reply.id}
-          navigation={navigation}
-        />
-      ))}
-    </View>
-  );
-}
-
-function ReplyDisplay({
-  reply,
-  layer = 0,
-  navigation,
-}: {
-  reply: Reply;
-  layer: number;
-  navigation: any;
-}) {
-  const [showChildren, setShowChildren] = React.useState(true);
-  const theme = useTheme();
-  const layerColors = useMemo(
-    () => [
-      theme.text,
-      theme.red,
-      theme.orange,
-      theme.yellow,
-      theme.green,
-      theme.teal,
-      theme.blue,
-      theme.indigo,
-      theme.purple,
-    ],
-    [],
-  );
-  return (
-    <View style={{ paddingLeft: 0 }}>
-      <View
-        style={{
-          paddingVertical: 8,
-          borderTopWidth: 0.5,
-          borderTopColor: theme.secondaryBackground,
-        }}
-      >
-        <Pressable
-          onPress={() => setShowChildren(s => !s)}
-          onLongPress={() => {
-            Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
-            navigation.navigate("Reply", {
-              id: reply.id,
-              title: reply.author.username,
-              html: reply.content_html,
-              type: "reply",
-            });
-          }}
-        >
-          <View
-            style={{
-              borderLeftWidth: 2,
-              borderColor: layerColors[layer % layerColors.length],
-              paddingLeft: 15,
-              paddingVertical: 3,
-            }}
-          >
-            <Text
-              style={{
-                color: theme.text,
-                fontSize: 16,
-                marginBottom: 5,
-                fontWeight: "500",
-              }}
-            >
-              {reply.author.username}
-              {"  "}
-              <Text style={{ color: theme.text, fontSize: 14 }}>
-                <Icon name="heart-outline" size={14} color={theme.text} light />{" "}
-                {reply.score}
-                {"   "}
-                <ElapsedTime time={reply.created} />
-              </Text>
-            </Text>
-            {showChildren && !!reply.content_html && (
-              <HTMLView
-                value={reply.content_html
-                  .replace(/<hr>/g, "")
-                  .replace(/\n/g, "")}
-                stylesheet={{
-                  p: { color: theme.text, fontSize: 16, fontWeight: "300" },
-                  h1: {
-                    color: theme.text,
-                    fontSize: 24,
-                    fontWeight: "300",
-                  },
-                }}
-              />
-            )}
-          </View>
-        </Pressable>
-      </View>
-      {reply.replies && reply.replies.items.length > 0 && showChildren && (
-        <View style={{ paddingLeft: 15 }}>
-          <RepliesDisplay
-            replies={reply.replies}
-            layer={layer + 1}
-            navigation={navigation}
-          />
-        </View>
-      )}
-    </View>
-  );
-}
