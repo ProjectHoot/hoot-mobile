fdf7fc1c6977710532c79313834afdc561e2cecf
Brandon Dyer <brandondyer64@gmail.com>
2021-10-02 13:03:18 -0500

    fixup

--- screens/FeedScreen.tsx
+++ screens/FeedScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import { StyleSheet, FlatList, Pressable } from "react-native";
 
 import PostDisplay from "../components/PostDisplay";
