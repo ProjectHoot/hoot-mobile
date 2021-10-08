84b73b07d31250aa5d62d36875797261bc587930
Brandon Dyer <brandondyer64@gmail.com>
2021-10-07 22:04:23 -0500

    Fixed communities list page scroll

--- components/CommunityFinder.tsx
+++ components/CommunityFinder.tsx
@@ -1,7 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { FlatList, Pressable, StyleSheet } from "react-native";
+import {
+  FlatList,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import Icon from "@expo/vector-icons/Ionicons";
-import { TextInput, View } from "./Themed";
 import * as LotideService from "../services/LotideService";
 import ActorDisplay from "./ActorDisplay";
 import useTheme from "../hooks/useTheme";
@@ -43,74 +49,91 @@ export default function CommunityFinder(props: CommunityFinderProps) {
         style={[
           styles.item,
           {
-            borderColor: theme.secondaryBackground,
+            borderColor: theme.tertiaryBackground,
           },
         ]}
       >
-        <ActorDisplay
-          name={item.name}
-          host={item.host}
-          local={item.local}
-          colorize={"always"}
-          newLine
-        />
-        <View style={{ display: "flex", flexDirection: "row" }}>
-          {item.you_are_moderator && (
-            <Icon name="shield-outline" size={20} color={theme.secondaryTint} />
-          )}
-          {item.your_follow?.accepted && (
-            <Icon
-              name="checkmark"
-              size={20}
-              color={theme.secondaryTint}
-              style={{ marginLeft: 5 }}
-            />
-          )}
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <ActorDisplay
+            name={item.name}
+            host={item.host}
+            local={item.local}
+            colorize={"always"}
+            newLine
+          />
+          <View style={{ display: "flex", flexDirection: "row" }}>
+            {item.you_are_moderator && (
+              <Icon
+                name="shield-outline"
+                size={20}
+                color={theme.secondaryTint}
+              />
+            )}
+            {item.your_follow?.accepted && (
+              <Icon
+                name="checkmark"
+                size={20}
+                color={theme.secondaryTint}
+                style={{ marginLeft: 5 }}
+              />
+            )}
+          </View>
         </View>
+        {!!item.description && item.description.length > 0 && (
+          <Text style={{ color: theme.secondaryText, marginTop: 10 }}>
+            {item.description?.substring(0, 120)}
+            {(item.description?.length || 0) > 120 && "..."}
+          </Text>
+        )}
       </Pressable>
     );
   };
 
   return (
-    <View>
-      <TextInput
-        placeholder={props.placeholder || "Filter communities"}
-        value={filterText}
-        onChangeText={setFilterText}
-        style={styles.input}
-      />
-      <FlatList
-        data={communitiesToDisplay}
-        renderItem={renderItem}
-        keyExtractor={(item, index) => `${item.id}-${index}`}
-        contentContainerStyle={styles.list}
-      />
-    </View>
+    <FlatList
+      data={communitiesToDisplay}
+      renderItem={renderItem}
+      keyExtractor={(item, index) => `${item.id}-${index}`}
+      contentContainerStyle={styles.list}
+      stickyHeaderIndices={[0]}
+      ListHeaderComponent={
+        <View style={{ backgroundColor: theme.background, padding: 15 }}>
+          <TextInput
+            placeholder={props.placeholder || "Filter communities"}
+            placeholderTextColor={theme.placeholderText}
+            value={filterText}
+            onChangeText={setFilterText}
+            style={[styles.input, { color: theme.text }]}
+          />
+        </View>
+      }
+    />
   );
 }
 
 const styles = StyleSheet.create({
-  root: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
   input: {
+    marginHorizontal: 25,
     paddingVertical: 10,
-    marginHorizontal: 20,
-    marginTop: 20,
   },
   list: {
-    padding: 20,
-    paddingBottom: 100,
+    paddingBottom: 50,
   },
   item: {
     display: "flex",
-    flexDirection: "row",
+    flexDirection: "column",
     justifyContent: "space-between",
-    alignItems: "center",
-    height: 50,
-    borderBottomWidth: 1,
+    alignItems: "stretch",
+    paddingVertical: 10,
+    borderTopWidth: StyleSheet.hairlineWidth || 1,
     paddingHorizontal: 20,
+    marginHorizontal: 20,
   },
 });
